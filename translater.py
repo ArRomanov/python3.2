a43d18f86c56ac7ab50ef242d383abf5c07eb730
Artem Romanov <romanov-artyom@bk.ru>
2020-05-31 20:57:11 +0300

    Method for files upload is ready

--- translater.py
+++ translater.py
@@ -1,9 +1,9 @@
+import os
+
 import requests
 
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
-API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
 
 def translate_and_write_to_file(input_file_path, output_file_path, from_lang, to_lang='ru'):
@@ -20,6 +20,9 @@ def translate_and_write_to_file(input_file_path, output_file_path, from_lang, to
     @param from_lang:
     @param to_lang:
     """
+    URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+
     with open(input_file_path, encoding='utf-8') as file:
         text = file.readlines()
 
@@ -35,9 +38,34 @@ def translate_and_write_to_file(input_file_path, output_file_path, from_lang, to
         output_file.write(''.join(response_content['text']))
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+def upload_translated_files_on_yadisk():
+    URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    OAUTH_KEY = ''  # https://yandex.ru/dev/disk/poligon/ - генерация OAuth-токен
+
+    all_files = os.listdir('.')
+    translated_files = filter(lambda x: x.endswith('translated.txt'), all_files)
+    for file in translated_files:
+        with open(file, 'rb') as upload_file:
+            params_for_get_url = {
+                'path': file,
+                'overwrite': True,
+            }
+            headers = {
+                'Authorization': OAUTH_KEY
+            }
+
+            generate_upload_link_response = requests.get(URL, headers=headers, params=params_for_get_url)
+            response_content = generate_upload_link_response.json()
+            upload_link = response_content['href']
+
+            files = {
+                'file': upload_file.read()
+            }
+            requests.put(upload_link, files=files)
+
 
 if __name__ == '__main__':
     translate_and_write_to_file('DE.txt', 'DE_translated.txt', 'de')
     translate_and_write_to_file('ES.txt', 'ES_translated.txt', 'es')
     translate_and_write_to_file('FR.txt', 'FR_translated.txt', 'fr')
+    upload_translated_files_on_yadisk()
